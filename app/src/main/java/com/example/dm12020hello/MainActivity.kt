9a979a5b082b2bc955080102a7f747df291d73b6
JPCASATI <jpcasati@usp.br>
2020-08-19 22:02:26 -0300

    Primeiro commit do projeto.

---
+++ app/src/main/java/com/example/dm12020hello/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.dm12020hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // MEU CÓDIGO
+
+    }
+}
