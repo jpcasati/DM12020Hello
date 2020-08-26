22f80b70ad4e68fdfdd8ec59e2333b2782b795ba
JPCASATI <jpcasati@usp.br>
2020-08-26 19:35:20 -0300

    Primeiro commit do projeto.

---
+++ app/src/main/java/com/example/dm12020hello/Tela2.kt
@@ -0,0 +1,15 @@
+package com.example.dm12020hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela2)
+
+        // MEU CÓDIGO
+
+    }
+}
